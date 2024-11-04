2aac344d4ca56dc081760f5fdeb78d704838a35b
Leonardo Abdala <leonardo-abdala@hotmail.com>
2024-11-04 00:02:25 -0300

    Finalizado o portfolio.

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+const toogleTheme = document.getElementById('toggle-theme');
+const rootHtml = document.documentElement;
+
+function changeTheme(){
+
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+    currentTheme === 'dark' ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
+    
+    toogleTheme.classList.toggle("bi-sun");
+    toogleTheme.classList.toggle("bi-moon-stars");
+}
+
+
+toogleTheme.addEventListener("click", changeTheme);
